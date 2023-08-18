277d76793fe6f478127ba2494b81bcd9bbd07685
Sean Simmons <ssimmons@progress.com>
2023-08-18 10:57:10 -0400

    didnt like that value either
    mac 10.15 cant seem to _see_ the cert, while mac os 11 says its invalid
    
    Signed-off-by: Sean Simmons <ssimmons@progress.com>

--- config/projects/chef-foundation.rb
+++ config/projects/chef-foundation.rb
@@ -78,7 +78,7 @@ end
 
 package :pkg do
   identifier "com.getchef.pkg.chef"
-  signing_identity "Developer ID Installer: Chef Software, Inc. (EU3VF8YLX2)"
+  signing_identity "Chef Software, Inc. (EU3VF8YLX2)"
 end
 compress :dmg
 
